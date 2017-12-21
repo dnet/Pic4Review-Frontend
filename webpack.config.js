9c2d5dc4a0fafc2787ef6a20859b22839a5c55df
PanierAvide <panieravide@riseup.net>
2017-12-21 13:00:27 +0100

    Fix again of webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ else {
 	plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
-plugins.push(new TransferWebpackPlugin([ {from: 'www'} ], path.resolve(__dirname, 'src'));
+plugins.push(new TransferWebpackPlugin([ {from: 'www'} ], path.resolve(__dirname, 'src')));
 
 const config = {
 	entry: {
