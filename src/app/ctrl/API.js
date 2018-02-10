e9ed37acd05c8c5a9288496f849e153e853aae8d
PanierAvide <panieravide@riseup.net>
2018-02-10 10:51:21 +0100

    Fix flat properties for OAPI

--- src/app/ctrl/API.js
+++ src/app/ctrl/API.js
@@ -37,7 +37,7 @@ class API {
 				else {
 					resolve(data);
 				}
-			}, { overpassUrl: CONST.OAPI_URL });
+			}, { overpassUrl: CONST.OAPI_URL, flatProperties: true });
 		});
 	}
 	
--- src/app/view/mission/MissionReviewTagsComponent.js
+++ src/app/view/mission/MissionReviewTagsComponent.js
@@ -13,8 +13,8 @@ class MissionReviewTagsComponent extends Component {
 	render() {
 		const style = Object.assign({}, this.props.style);
 		
-		const tags = Object.keys(this.props.feature.properties).map(k => {
-			return <TableRow key={k}>
+		const tags = Object.keys(this.props.feature.properties).map((k, i) => {
+			return <TableRow key={i}>
 				<TableCell>{k}</TableCell>
 				<TableCell>{this.props.feature.properties[k]}</TableCell>
 			</TableRow>;
