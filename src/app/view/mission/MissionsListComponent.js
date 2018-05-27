a97964f872d2163fc7cb66d67596df08c2b781dc
PanierAvide <panieravide@riseup.net>
2018-05-27 13:11:08 +0200

    Fix illustration link in mission list

--- src/app/view/mission/MissionsListComponent.js
+++ src/app/view/mission/MissionsListComponent.js
@@ -32,7 +32,7 @@ class MissionsListComponent extends Component {
 						</div>
 						{showPic &&
 							<div style={{
-								background: "url(https://wiki.openstreetmap.org/w/images/2/2f/Pelican.jpg)",
+								background: "url("+m.options.illustration+")",
 								backgroundSize: "cover",
 								height: 150,
 								minWidth: 150,
