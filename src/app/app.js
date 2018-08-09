f16e299756fc86182441a31e3bebc34a901a5f68
PanierAvide <panieravide@riseup.net>
2018-08-09 07:46:40 +0200

    Updated locales, added german

--- src/app/app.js
+++ src/app/app.js
@@ -9,7 +9,7 @@ import I18n from 'i18nline/lib/i18n';
 import OsmAuth from 'osm-auth';
 import PubSub from 'pubsub-js';
 
-const LOCALES = [ "en", "fr", "hu", "pt-PT", "it" ];
+const LOCALES = [ "de", "en", "fr", "hu", "it", "pt-PT" ];
 
 const readURLParams = str => {
 	const u = str.split('?');
